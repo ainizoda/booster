dfff7bc461bda9ae14f44f281bf4d09f78d390d2
ainizoda <ainizodam@gmail.com>
2024-05-18 18:07:45 +0500

    changes

---
+++ src/components/avatar.tsx
@@ -0,0 +1,24 @@
+import { FC, useMemo } from "react";
+import cls from "classnames";
+import { useWebAppData } from "../contexts";
+
+type Props = {
+  className?: string;
+};
+export const Avatar: FC<Props> = ({ className }) => {
+  const { user } = useWebAppData();
+  const shortName = useMemo(
+    () => (user?.first_name[0] || "") + (user?.last_name?.[0] || ""),
+    [user]
+  );
+  return (
+    <div
+      className={cls(
+        "bg-[#266FF1] w-32 h-32 flex justify-center items-center rounded-full text-5xl",
+        className
+      )}
+    >
+      {shortName}
+    </div>
+  );
+};
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,27 +1,20 @@
 import { Button } from "../components/ui/button";
-import { Avatar } from "../components/ui/avatar";
-import { useWebAppData } from "../contexts";
-import { useMemo } from "react";
+import { Avatar } from "../components/avatar";
 import { Input } from "../components/ui/input";
 
 export default function RegisterPage() {
-  const { user } = useWebAppData();
-  const shortName = useMemo(
-    () => (user?.first_name[0] || "") + (user?.last_name?.[0] || ""),
-    [user]
-  );
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <div className="mt-24 w-full">
-        <div className="text-3xl text-center">
+        <div className="text-3xl text-center font-bold">
           Boom!
           <br />
           Welcome to the Crew
         </div>
         <div className="mt-10 flex justify-center">
-          <Avatar name={shortName || "B"} />
+          <Avatar />
         </div>
-        <div className="mt-12 mb-12 leading-8 text-[#ffffffd8]">
+        <div className="mt-12 mb-14 leading-8 text-[#ffffffd8]">
           <div>&nbsp;Nickname</div>
           <Input placeholder="choose your nickname" />
         </div>
