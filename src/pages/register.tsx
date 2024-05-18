af8c7ad312fe82e861f613e1c2ce461de792c9e3
ainizoda <ainizodam@gmail.com>
2024-05-18 17:01:44 +0500

    changes

---
+++ src/pages/register.tsx
@@ -0,0 +1,32 @@
+import { Button } from "../components/ui/button";
+import boosterLogo from "../assets/booster.svg";
+import { Avatar } from "../components/ui/avatar";
+import { useWebAppData } from "../contexts";
+import { useMemo } from "react";
+
+export default function RegisterPage() {
+  const { user } = useWebAppData();
+  const shortName = useMemo(
+    () => (user?.first_name[0] || "") + (user?.last_name?.[0] || ""),
+    [user]
+  );
+  return (
+    <div className="flex flex-col justify-center items-center h-screen flex-wrap">
+      <div className="mt-24 text-center">
+        <div className="text-3xl">
+          Boom!
+          <br />
+          Welcome to the Crew
+        </div>
+        <div className="mt-10 flex justify-center">
+          <Avatar name={shortName} />
+        </div>
+        <div className="mt-5 leading-8 text-[#ffffffd8]">
+          Where every tap turns into treasure. Ready to make your mark and bloom
+          with us? Let’s go!
+        </div>
+      </div>
+      <Button className="mt-auto mb-6">Continue</Button>
+    </div>
+  );
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,9 @@ import { lazy } from "react";
 import { RouteObject } from "react-router";
 
 const WelcomePage = lazy(() => import("./pages/welcome"));
+const RegisterPage = lazy(() => import("./pages/register"));
 
 export const routerConfig: RouteObject[] = [
   { path: "/", element: <WelcomePage /> },
+  { path: "/register", element: <RegisterPage /> },
 ];
