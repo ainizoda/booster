aa432e93c5fc652aef501795a7191bc5880f433f
ainizoda <ainizodam@gmail.com>
2024-05-18 08:35:21 +0500

    changes

--- src/utils/paramsToObject.ts
+++ src/utils/paramsToObject.ts
@@ -1,8 +1,8 @@
 import { TelegramWebApps } from "telegram-webapps-types";
 
-export function paramsToObject(params: string) {
+export function paramsToObject(params: string): TelegramWebApps.WebAppUser {
   const p = new URLSearchParams(params);
-  const result: TelegramWebApps.WebAppInitData = {};
+  const result: any = {};
   for (const [key, value] of p) {
     if (value.startsWith("{")) {
       result[key] = JSON.parse(value);
