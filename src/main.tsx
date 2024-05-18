929ca3ffcf97f5acd2ce12265cb5f4171f904c62
ainizoda <ainizodam@gmail.com>
2024-05-18 11:05:51 +0500

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,6 @@
 import ReactDOM from "react-dom/client";
 // import App from "./App.tsx";
 import "./index.scss";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { routerConfig } from "./routes.tsx";
-import { TelegramProvider } from "./contexts/telegram.tsx";
 import App from "./App.tsx";
 
-// const router = createBrowserRouter(routerConfig);
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <TelegramProvider>
-    <App />
-  </TelegramProvider>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
