582ecb8238a58e086f17f24dbeff72dfdb91f547
ainizoda <ainizodam@gmail.com>
2024-05-27 15:41:19 +0500

    .

--- src/components/layouts/sidebar.tsx
+++ src/components/layouts/sidebar.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useRef } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router";
 import { HomeIcon, ReferralsIcon, TasksIcon } from "../icons";
 
@@ -6,9 +6,16 @@ export const Sidebar: FC = () => {
   const router = useLocation();
   const navigate = useNavigate();
   const screenRef = useRef<HTMLDivElement | null>(null);
+  const [keyboardVisible, setKeyboardVisible] = useState(false);
 
   useEffect(() => {
+    const windowHeight = window.innerHeight;
     const handleResize = () => {
+      if (window.innerHeight !== windowHeight) {
+        setKeyboardVisible(true);
+      } else {
+        setKeyboardVisible(false);
+      }
       screenRef?.current?.scrollTo(0, 1000);
     };
     window.addEventListener("resize", handleResize);
@@ -19,31 +26,33 @@ export const Sidebar: FC = () => {
       <div className="h-full overflow-auto" ref={screenRef}>
         <Outlet />
       </div>
-      <div className="fixed bottom-0 left-0 w-full px-14 py-4 bg-black">
-        <div className="w-full flex items-center justify-between">
-          <div
-            className="flex flex-col items-center"
-            onClick={() => navigate("/home")}
-          >
-            <HomeIcon active={router.pathname.includes("/home")} />
-            <small className="mt-1">Home</small>
-          </div>
-          <div
-            className="flex flex-col items-center"
-            onClick={() => navigate("/tasks")}
-          >
-            <TasksIcon active={router.pathname.includes("/tasks")} />
-            <small className="mt-1">Tasks</small>
-          </div>
-          <div
-            className="flex flex-col items-center"
-            onClick={() => navigate("/referrals")}
-          >
-            <ReferralsIcon active={router.pathname.includes("/referrals")} />
-            <small className="mt-1">Referrals</small>
+      {!keyboardVisible && (
+        <div className="fixed bottom-0 left-0 w-full px-14 py-4 bg-black">
+          <div className="w-full flex items-center justify-between">
+            <div
+              className="flex flex-col items-center"
+              onClick={() => navigate("/home")}
+            >
+              <HomeIcon active={router.pathname.includes("/home")} />
+              <small className="mt-1">Home</small>
+            </div>
+            <div
+              className="flex flex-col items-center"
+              onClick={() => navigate("/tasks")}
+            >
+              <TasksIcon active={router.pathname.includes("/tasks")} />
+              <small className="mt-1">Tasks</small>
+            </div>
+            <div
+              className="flex flex-col items-center"
+              onClick={() => navigate("/referrals")}
+            >
+              <ReferralsIcon active={router.pathname.includes("/referrals")} />
+              <small className="mt-1">Referrals</small>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
