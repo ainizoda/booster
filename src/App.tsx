1907eef88b6abd206a64977caf6310e6a5e78c30
ainizoda <ainizodam@gmail.com>
2024-06-18 22:36:40 +0500

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import { Suspense } from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
-import { TelegramProvider, useWebAppData } from "./contexts";
+import { TelegramProvider, useWebAppInitData } from "./contexts";
 import { routerConfig } from "./routes";
 import { Loader } from "./components";
 
 import "./App.scss";
+import { paramsToObject } from "./utils";
 
 const router = createBrowserRouter(routerConfig);
 
 function App() {
-  alert(JSON.stringify(useWebAppData()));
+  alert(JSON.stringify(paramsToObject(useWebAppInitData())));
   return (
     <TelegramProvider>
       <Toaster position="top-center" reverseOrder={false} />
