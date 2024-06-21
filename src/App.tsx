d54132cf3d0acb1907425c7f8edd1dfa15dd94cc
ainizoda <ainizodam@gmail.com>
2024-06-21 15:52:08 +0500

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { ErrorBoundary, FallbackProps } from "react-error-boundary";
 
 import { TelegramProvider } from "./contexts";
 import { routerConfig } from "./routes";
@@ -11,13 +12,23 @@ import "./App.scss";
 const router = createBrowserRouter(routerConfig);
 
 function App() {
+  function fallbackRender({ error }: FallbackProps) {
+    return (
+      <div role="alert">
+        <p>Something went wrong:</p>
+        <pre style={{ color: "red" }}>{error.message}</pre>
+      </div>
+    );
+  }
   return (
-    <TelegramProvider>
-      <Toaster position="top-center" reverseOrder={false} />
-      <Suspense fallback={<Loader />}>
-        <RouterProvider router={router} />
-      </Suspense>
-    </TelegramProvider>
+    <ErrorBoundary fallbackRender={fallbackRender}>
+      <TelegramProvider>
+        <Toaster position="top-center" reverseOrder={false} />
+        <Suspense fallback={<Loader />}>
+          <RouterProvider router={router} />
+        </Suspense>
+      </TelegramProvider>
+    </ErrorBoundary>
   );
 }
 
