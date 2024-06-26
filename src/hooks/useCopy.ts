b62678f69d6d6f1031e2ad64fa85dab34674174d
ainizoda <ainizodam@gmail.com>
2024-06-26 16:45:13 +0500

    .

--- src/hooks/useCopy.ts
+++ src/hooks/useCopy.ts
@@ -4,7 +4,9 @@ import { toast } from "../lib";
 export const useCopy = () => {
   const isSafari = () => navigator.userAgent.match(/ipad|iphone|Mac OS/i);
   const copyTextToClipboard = useCallback(async (text: string) => {
-    text = `link - ${text.replace("https://", "")}`;
+    if (text.includes("https://")) {
+      text = `link - ${text.replace("https://", "")}`;
+    }
     try {
       if (navigator.clipboard && navigator.clipboard.writeText) {
         await navigator.clipboard.writeText(text);
