72e39d972482ed11fa8f10bbd1c7ac205200e29e
ainizoda <ainizodam@gmail.com>
2024-06-26 15:46:31 +0500

    .

--- src/hooks/useCopy.ts
+++ src/hooks/useCopy.ts
@@ -1,35 +1,33 @@
-import copy from "copy-to-clipboard";
 import { useCallback } from "react";
-// import { toast } from "../lib";
+import { toast } from "../lib";
 
 export const useCopy = () => {
-  const copyTextToClipboard = useCallback((text: string) => {
-    // text = text.replace("https://", "");
-    // try {
-    //   // Use the Clipboard API if available
-    //   if (navigator.clipboard && navigator.clipboard.writeText) {
-    //     await navigator.clipboard.writeText(text);
-    //   } else {
-    //     // Fallback for older browsers
-    //     const textarea = document.createElement("textarea");
-    //     textarea.value = text;
-    //     textarea.style.position = "fixed"; // Avoid scrolling to the bottom of the page
-    //     textarea.style.top = "0";
-    //     textarea.style.left = "0";
-    //     textarea.style.opacity = "0"; // Invisible but still selectable
-    //     document.body.appendChild(textarea);
-    //     textarea.focus();
-    //     textarea.select();
-    //     const successful = document.execCommand("copy");
-    //     document.body.removeChild(textarea);
-    //     if (!successful) {
-    //       throw new Error("Failed to copy text");
-    //     }
-    //   }
-    // } catch (err) {
-    //   toast("Failed to copy text", { error: true });
-    // }
-    copy(text);
+  const copyTextToClipboard = useCallback(async (text: string) => {
+    text = text.replace("https://", "");
+    try {
+      // Use the Clipboard API if available
+      if (navigator.clipboard && navigator.clipboard.writeText) {
+        await navigator.clipboard.writeText(text);
+      } else {
+        // Fallback for older browsers
+        const textarea = document.createElement("textarea");
+        textarea.value = text;
+        textarea.style.position = "fixed"; // Avoid scrolling to the bottom of the page
+        textarea.style.top = "0";
+        textarea.style.left = "0";
+        textarea.style.opacity = "0"; // Invisible but still selectable
+        document.body.appendChild(textarea);
+        textarea.focus();
+        textarea.select();
+        const successful = document.execCommand("copy");
+        document.body.removeChild(textarea);
+        if (!successful) {
+          throw new Error("Failed to copy text");
+        }
+      }
+    } catch (err) {
+      toast("Failed to copy text", { error: true });
+    }
   }, []);
 
   return copyTextToClipboard;
